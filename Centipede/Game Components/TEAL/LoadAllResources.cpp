cc74a0716bfa318468734f5ad1dc30da729d114e
Ryan Campbell <RCAMPB27@mail.depaul.edu>
2018-09-12 17:06:50 -0500

    Removed legacy sounds and sprites and added centipede sounds and sprites

--- Centipede/Game Components/TEAL/LoadAllResources.cpp
+++ Centipede/Game Components/TEAL/LoadAllResources.cpp
@@ -5,10 +5,38 @@
 
 #include "ResourceManager.h"
 #include "../Splash.h"
+//#include "../../reference/Asset/Sounds/"
+//#include "../../reference/Asset/Sprites/"
 
 void ResourceManager::LoadAllResources()
 {
 	SetStartScene( new Splash() );
 
+	AddTexture("Blaster", "blaster.png");
+	AddTexture("Bullet", "bullet.png");
+	AddTexture("CentipedeBody", "centipede_body.png");
+	AddTexture("CentipedeHead", "centipede_head.png");
+	AddTexture("Death", "death.png");
+	AddTexture("Flea", "flea.png");
+	AddTexture("Mushroom", "mushroom.png");
+	AddTexture("Scorpion", "scorpion.png");
+	AddTexture("Spawn", "spawn.png");
+	AddTexture("Spider", "spider.png");
+
+	AddSound("Fire1", "fire1.wav");
+
+	//AddTexture("Spaceship", "SpaceShip2.png");
+	//AddTexture("Shots", "ColoredShots.png");
+	//AddTexture("Mine", "Mine.png");
+	//AddTexture("Asteroids", "asteroids.png");
+	//AddTexture("Explosion", "Explosion.png");
+
+	//AddTexture("Splashpage", "DemoSplash.jpg", false);
+
+	//AddSound("Boom", "bazooka3.wav");
+	//AddSound("Zap", "scifiblast.wav");
+	//AddSound("Ding", "glass.wav");
+
+	//AddFont("PointFont", "MotorwerkOblique.ttf");
 }
 
