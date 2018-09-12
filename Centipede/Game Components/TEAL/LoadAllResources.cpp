e2d3b9d1329b161fce039dc65fce13d3619a4bb0
Ryan Campbell <RCAMPB27@mail.depaul.edu>
2018-09-12 14:09:42 -0500

    Test. Remove files

--- Centipede/Game Components/TEAL/LoadAllResources.cpp
+++ Centipede/Game Components/TEAL/LoadAllResources.cpp
@@ -10,18 +10,5 @@ void ResourceManager::LoadAllResources()
 {
 	SetStartScene( new Splash() );
 
-	AddTexture("Spaceship", "SpaceShip2.png");
-	AddTexture("Shots", "ColoredShots.png");
-	AddTexture("Mine", "Mine.png");
-	AddTexture("Asteroids", "asteroids.png");
-	AddTexture("Explosion", "Explosion.png");
-
-	AddTexture("Splashpage", "DemoSplash.jpg", false);
-	
-	AddSound("Boom", "bazooka3.wav");
-	AddSound("Zap", "scifiblast.wav");
-	AddSound("Ding", "glass.wav");
-
-	AddFont("PointFont", "MotorwerkOblique.ttf" );
 }
 
